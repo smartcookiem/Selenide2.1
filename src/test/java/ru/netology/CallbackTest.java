99ffdf084b28c36c0f161f1cc28bd6e25efd4858
smartcookiem <svyaznoy1835@gmail.com>
2023-07-26 00:18:04 +0300

    Final commit

---
+++ src/test/java/ru/netology/CallbackTest.java
@@ -0,0 +1,20 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CallbackTest {
+
+    @Test
+    void shouldTestForm() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").setValue("Анисия Федорова");
+        $("[data-test-id=phone] input").setValue("+79216482049");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
